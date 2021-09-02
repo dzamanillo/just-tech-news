d64c4e4c0036c25eb253000c5c025a9402f8f5b9
Daniel Zamanillo <dzamanillok@gmail.com>
2021-09-02 11:38:44 -0500

    homepage built

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-
 const apiRoutes = require("./api");
+const homeRoutes = require("./home_routes");
 
 router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
 
 router.use((req, res) => {
 	res.status(404).end();
