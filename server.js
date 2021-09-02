afa9d34531e16ad05a4de644a4bc904f8c94be5b
Daniel Zamanillo <dzamanillok@gmail.com>
2021-09-02 16:22:20 -0500

    server cleaned  up

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const session = require("express-session");
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -28,14 +27,11 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// turn on routes
-const routes = require("./controllers");
-app.use(routes);
+app.use(require("./controllers/"));
 
-// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log("Now listening"));
 });
